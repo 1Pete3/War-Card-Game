0a0b7972e2f64886ab4831cc8c4212a743de1091
Peter <1pete3learning@gmail.com>
2023-08-08 05:48:39 -0700

    War Card game with alert after winning/losing

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -8,14 +8,94 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var playerCard = "card13"
+    @State var cpuCard = "card13"
+    
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
+    @State var result = false
+    @State var resultText = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack{
+            Image("background-plain")
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                Button{
+                    deal()
+                    if(playerScore ==  20){
+                        result = true
+                        playerScore = 0
+                        cpuScore = 0
+                        resultText = "You won! 🏆"
+                    }
+                    else if (cpuScore == 20){
+                        result = true
+                        playerScore = 0
+                        cpuScore = 0
+                        resultText = "You lost 😔"
+                    }
+                   
+                    
+                }label:{
+                    Image("button")
+                }.alert( isPresented: $result){
+                    Alert(
+                    title: Text(resultText)
+                    )
+                }
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom,10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom,10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
         }
-        .padding()
+        .foregroundColor(.white)
+        
+    }
+    
+    func deal(){
+        var playerCardValue = Int.random(in: 2...14)
+        playerCard = "card" + String(playerCardValue)
+        
+        var cpuCardValue = Int.random(in: 2...14)
+        cpuCard = "card" + String(cpuCardValue)
+        
+        if(playerCardValue > cpuCardValue){
+            playerScore += 1
+        }
+        else if (cpuCardValue > playerCardValue){
+            cpuScore += 1
+        }
+        
     }
 }
 
